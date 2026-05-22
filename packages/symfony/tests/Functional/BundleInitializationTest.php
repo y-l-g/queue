a5dfef32fd2e9cd50cd364caf93314b68df146a9
y-l-g <youenn.le.gouedec@gmail.com>
2026-05-22 13:53:19 +0200

    Run Symfony functional bundle tests

--- packages/symfony/tests/Functional/BundleInitializationTest.php
+++ packages/symfony/tests/Functional/BundleInitializationTest.php
@@ -4,25 +4,38 @@ declare(strict_types=1);
 
 namespace Pogo\Queue\Symfony\Tests\Functional;
 
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use Pogo\Queue\Symfony\Tests\App\Kernel;
 use Pogo\Queue\Symfony\Transport\PogoQueueTransportFactory;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-final class BundleInitializationTest extends TestCase
+final class BundleInitializationTest extends KernelTestCase
 {
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+
+    protected static function createKernel(array $options = []): KernelInterface
+    {
+        return new Kernel($options['environment'] ?? 'test', false);
+    }
+
+    #[RunInSeparateProcess]
     public function testBundleServicesAreRegistered(): void
     {
-        $kernel = new Kernel('test', true);
-        $kernel->boot();
+        try {
+            self::bootKernel();
+            $container = static::getContainer();
 
-        $container = $kernel->getContainer();
-
-        $this->assertTrue($container->has('messenger.transport_factory'));
-
-
-        $transportFactory = $container->get(PogoQueueTransportFactory::class);
-        $this->assertInstanceOf(PogoQueueTransportFactory::class, $transportFactory);
-        $this->assertInstanceOf(TransportFactoryInterface::class, $transportFactory);
+            $this->assertTrue($container->has(PogoQueueTransportFactory::class));
+            $transportFactory = $container->get(PogoQueueTransportFactory::class);
+            $this->assertInstanceOf(PogoQueueTransportFactory::class, $transportFactory);
+            $this->assertInstanceOf(TransportFactoryInterface::class, $transportFactory);
+        } finally {
+            self::ensureKernelShutdown();
+        }
     }
 }
